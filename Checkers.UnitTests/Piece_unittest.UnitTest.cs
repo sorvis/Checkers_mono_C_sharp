18d1230e7eaa979c8c6e215f7b12b08c23131453
Steven Orvis <steven.orvis@geneva.edu>
2012-08-15 21:59:11 -0400

    started checkers game

---
+++ Checkers.UnitTests/Piece_unittest.UnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using NUnit.Framework;
+
+namespace Checkers.UnitTests
+{
+	[TestFixture]
+	public class Piece_unittest
+	{
+		public Piece_unittest()
+		{
+		}
+	}
+}
+
---
+++ Checkers/Piece.cs
@@ -0,0 +1,18 @@
+using System;
+using NUnit.Framework;
+
+namespace Checkers
+{
+	public class Piece
+	{
+		public Piece ()
+		{
+		}
+
+		public void test_method()
+		{
+
+		}
+	}
+}
+
