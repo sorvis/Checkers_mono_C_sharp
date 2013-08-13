9a671b7f283340d612573f79127070bdb81fbf97
Steven Orvis <steven@trappingpa.com>
2013-08-13 19:32:24 -0400

    Added Checkers.UI

---
+++ Checkers.UI/Checkers.UI/Board.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Checkers.UI
+{
+    class Board : Grid
+    {
+        public int Columns 
+        {
+            get { return ColumnDefinitions.Count; }
+            set {setUpPanels(value, Rows); } 
+        }
+
+        public int Rows
+        {
+            get { return RowDefinitions.Count; }
+            set { setUpPanels(Columns, value);}
+        }
+
+        private IValueConverter _converter;
+        public IValueConverter Converter
+        {
+            get { return _converter; }
+            set
+            {
+                _converter = value;
+                setUpPanels(Columns, Rows);
+            }
+        }
+
+        private void setUpPanels(int colums, int rows)
+        {
+            if (colums > 0 && rows > 0)
+            {
+                
+            }
+        }
+    }
+}
