3ccea748b8134e0d553d5d6d07a2a7bf9476d137
Steven Orvis <steven@trappingpa.com>
2013-08-14 19:55:25 -0400

    Added converter for color enum to Brushes

---
+++ Checkers.UI/Checkers.UI/Converters/ColorEnumToBrushesConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Checkers.UI.Converters
+{
+    class ColorEnumToBrushesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null && value is BlackWhiteColor)
+            {
+                switch((BlackWhiteColor) value)
+                {
+                    case BlackWhiteColor.Black:
+                        return Brushes.Black;
+                    case BlackWhiteColor.White:
+                        return Brushes.White;
+                }
+            }
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
